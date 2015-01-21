a6c9bb3bb8cc243fddb1a898b0cab7ffdc54cf35
Patrick Kivits <patrick@patrickkivits.nl>
2015-01-21 23:50:37 +0100

    Update guldencoin.py
    Changed explorer url prefixes

--- p2pool/bitcoin/networks/guldencoin.py
+++ p2pool/bitcoin/networks/guldencoin.py
@@ -20,9 +20,9 @@ POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoW
 BLOCK_PERIOD = 150 # s
 SYMBOL = 'NLG'
 CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'guldencoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/guldencoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.guldencoin'), 'guldencoin.conf')
-BLOCK_EXPLORER_URL_PREFIX = 'http://guldenchain.mycryptoco.in/block/'
-ADDRESS_EXPLORER_URL_PREFIX = 'http://guldenchain.mycryptoco.in/address/'
-TX_EXPLORER_URL_PREFIX = 'http://guldenchain.mycryptoco.in/tx/'
+BLOCK_EXPLORER_URL_PREFIX = 'https://explorer.guldencoin.com/#/block/'
+ADDRESS_EXPLORER_URL_PREFIX = 'https://explorer.guldencoin.com/#/address/'
+TX_EXPLORER_URL_PREFIX = 'https://explorer.guldencoin.com/#/transaction/'
 SANE_TARGET_RANGE = (2**256//1000000000 - 1, 2**256//1000 - 1)
 DUMB_SCRYPT_DIFF = 2**16
 DUST_THRESHOLD = 0.03e8
